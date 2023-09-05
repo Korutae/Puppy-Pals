d14634cc19f349cce6d8a280aca727032a302d97
Sarah Eis <Korutae4@gmail.com>
2023-09-05 11:41:18 -0700

    made a list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import {puppyList} from './data.js'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log(useState(puppyList));
 
   return (
     <>
       <div>
+        {
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name}</p>
+          })
+        }
       </div>
     </>
   )
